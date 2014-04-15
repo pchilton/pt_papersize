efbf745aa5c13c4609984482760eeba17a5db04b
Paul Chilton <paul.chilton@gmail.com>
2014-04-15 21:40:30 +1200

    Minimal calculation. Can only return 'Custom' or 'A4'

--- lib/pt/papersize.rb
+++ lib/pt/papersize.rb
@@ -1,4 +1,5 @@
 require 'pt/papersize/version'
+require 'pt/papersize/paper_sizes'
 
 module PT
   class PaperSize
@@ -8,7 +9,23 @@ module PT
     end
 
     def name
-      'Custom'
+      short_edge_pt = [@width,@height].min
+      long_edge_pt = [@width,@height].max
+
+      short_edge_mm = (short_edge_pt.to_f / 72.0) * 25.4
+      long_edge_mm = (long_edge_pt.to_f / 72.0) * 25.4
+
+      short_edge_floor = short_edge_mm.floor
+      short_edge_ceil = short_edge_mm.ceil
+
+      long_edge_floor = long_edge_mm.floor
+      long_edge_ceil = long_edge_mm.ceil
+
+      available_sizes = PT::PaperSize::PAPER_SIZES.select do |entry|
+        (entry[:width] == short_edge_floor || entry[:width] == short_edge_ceil) && (entry[:height] == long_edge_floor || entry[:height] == long_edge_ceil)
+      end
+
+      available_sizes.length > 0 ? available_sizes.first[:name] : 'Custom'
     end
   end
 end
---
+++ lib/pt/papersize/paper_sizes.rb
@@ -0,0 +1,7 @@
+module PT
+  class PaperSize
+    PAPER_SIZES = [
+      { name: 'A4', standard: 'ISO', width: 210, height: 297 },
+    ]
+  end
+end
