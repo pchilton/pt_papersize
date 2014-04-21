550bd9c6bf6a619a1df5ed147187f9612bf80ea2
Paul Chilton <paul.chilton@gmail.com>
2014-04-21 22:09:11 +1200

    Switch from new/object methods to find/hash return

--- lib/pt/papersize.rb
+++ lib/pt/papersize.rb
@@ -3,31 +3,27 @@ require 'pt/papersize/paper_sizes'
 
 module PT
   class PaperSize
-    def initialize(width, height)
-      @width = width
-      @height = height
-    end
+    def self.find(width, height, units = :pt)
+      short_edge = [width, height].min
+      long_edge = [width, height].max
 
-    def name
-      short_edge_pt = [@width,@height].min
-      long_edge_pt = [@width,@height].max
-
-      short_edge_mm = (short_edge_pt.to_f / 72.0) * 25.4
-      long_edge_mm = (long_edge_pt.to_f / 72.0) * 25.4
-
-      short_edge_floor = short_edge_mm.floor
-      short_edge_ceil = short_edge_mm.ceil
-
-      long_edge_floor = long_edge_mm.floor
-      long_edge_ceil = long_edge_mm.ceil
-
-      available_sizes = PT::PaperSize::PAPER_SIZES.select do |entry|
-        paper_short = [entry[:width], entry[:height]].min
-        paper_long = [entry[:width], entry[:height]].max
-        (paper_short == short_edge_floor || paper_short == short_edge_ceil) && (paper_long == long_edge_floor || paper_long == long_edge_ceil)
+      case units
+      when :pt, :point, :points
+        short_edge_mm = ((short_edge.to_f / 72.0) * 25.4).round
+        long_edge_mm = ((long_edge.to_f / 72.0) * 25.4).round
+      when :mm, :millimetre, :millimetres
+        short_edge_mm = short_edge
+        long_edge_mm = long_edge
+      when :inch, :inches
+        short_edge_mm = (short_edge.to_f * 25.4).round
+        long_edge_mm = (long_edge.to_f * 25.4).round
       end
 
-      available_sizes.length > 0 ? available_sizes.first[:name] : 'Custom'
+      available_sizes = PT::PaperSize::PAPER_SIZES.select do |entry|
+        (short_edge_mm == entry[:width]) && (long_edge_mm == entry[:height])
+      end
+
+      available_sizes.length > 0 ? available_sizes.first : { name: 'Custom', width: short_edge_mm, height: long_edge_mm, units: :mm}
     end
   end
 end
--- spec/pt_papersize_spec.rb
+++ spec/pt_papersize_spec.rb
@@ -1,15 +1,18 @@
 require 'pt/papersize'
 
 describe PT::PaperSize do
-  it "names ISO A4" do
-    pt_papersize = PT::PaperSize.new(595,842)
-    pt_papersize.name.should eq('A4')
-    pt_papersize = PT::PaperSize.new(842,595)
-    pt_papersize.name.should eq('A4')
+  it "names ISO A4 from PostScript points" do
+    PT::PaperSize.find(595,842,:pt)[:name].should eq('A4')
+    PT::PaperSize.find(842,595,:pt)[:name].should eq('A4')
+  end
+
+  it "names SRA3 from various units" do
+    PT::PaperSize.find(320,450,:mm)[:name].should eq('SRA3')
+    PT::PaperSize.find(907,1276,:pt)[:name].should eq('SRA3')
+    PT::PaperSize.find(12.6,17.7,:inch)[:name].should eq('SRA3')
   end
 
   it "names random size as Custom" do
-    pt_papersize = PT::PaperSize.new(120,20)
-    pt_papersize.name.should eq('Custom')
+    PT::PaperSize.find(120,20,:mm)[:name].should eq('Custom')
   end
 end
